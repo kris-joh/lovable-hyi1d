8904a49f8bb62eaf77eb032a86fc31d808cb9950
Christo John <christopulimoottil@gmail.com>
2025-06-22 13:46:30 +0530

    Initial commit (src/App.tsx)

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-3xl font-bold">Fridge Photo Analyzer</h1>
+      {/* Additional components and logic will go here */}
+    </div>
+  );
+}
+
+export default App;
